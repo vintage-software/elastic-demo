498a175e35bb3753227226a80062f42aeb59897a
Steve Haar <info@stevehaar.com>
2017-03-08 01:18:01 -0600

    refactor

--- base-elastic.ts
+++ base-elastic.ts
@@ -1,50 +1,71 @@
-import * as request from 'request-promise';
-import { RequestPromise } from 'request-promise';
-import { Thenable, join } from 'bluebird';
+import fetch from 'node-fetch';
+import { Response } from 'node-fetch';
 
 export class BaseElastic {
-    protected baseUrl = 'http://localhost:9200';
+    private baseUrl = 'http://localhost:9200';
 
-    protected indexUrl = `${this.baseUrl}/test`;
+    private indexUrl = `${this.baseUrl}/test`;
 
     protected typeUrl = `${this.indexUrl}/widget`
 
-    protected recreateIndex(mapping: any): Thenable<void> {
+    protected recreateIndex(mapping: any) {
         return this.deleteIndex()
             .then(() => this.createIndex(mapping));
     }
 
-    protected execute(message: string, request: RequestPromise): Thenable<void> {
+    protected indexDocuments(documents: any[]) {
+        return Promise.all(documents.map(doc => this.indexDocument(doc)))
+            .then(() => this.refreshIndex());
+    }
+
+    protected search(query: any) {
+        return this.execute('search', this.http('POST', `${this.typeUrl}/_search`, query));
+    }
+
+    protected execute(message: string, request: Promise<any>) {
         return request
-            .then(() => console.log(`${message}: success`))
-            .catch(() => console.log(`${message}: failed`));
+            .then(result => {
+                console.log(`${message}: success`);
+                return result;
+            })
+            .catch(result => {
+                console.log(`${message}: failure`);
+                return Promise.resolve(undefined);
+            });
     }
 
     private deleteIndex() {
-        return this.execute('delete index', request.delete(this.indexUrl));
+        return this.execute('delete index', this.http('DELETE', this.indexUrl));
     }
 
     private createIndex(mapping: any) {
-        return this.execute('create index', request.put(this.indexUrl, {
-            json: {
-                settings: {
-                    number_of_shards: 1,
-                    number_of_replicas: 0
-                },
-                mappings: {
-                    widget: mapping
-                }
-            }
-        }));
-    }
-
-    indexDocuments(documents: any[]) {
-        return join(documents.map(doc => this.indexDocument(doc)));
+        return this.execute('create index', this.http('PUT', this.indexUrl, this.getIndexSettings(mapping)));
     }
 
     private indexDocument(document) {
-        return this.execute('insert doc', request.post(this.typeUrl, {
-            json: document
-        }))
+        return this.execute('insert doc', this.http('POST', this.typeUrl, document));
+    }
+
+    private refreshIndex() {
+        return this.execute('refresh', this.http('POST', `${this.indexUrl}/_refresh`));
+    }
+
+    private getIndexSettings(mapping: any) {
+        return {
+            settings: {
+                number_of_shards: 1,
+                number_of_replicas: 0
+            },
+            mappings: {
+                widget: mapping
+            }
+        };
+    }
+
+    private http(method: string, url: string, body?: any): Promise<any> {
+        return fetch(url, {
+            method: method,
+            body: JSON.stringify(body)
+        }).then(response => response && response.json());
     }
 }
--- basic-search.ts
+++ basic-search.ts
@@ -1,17 +1,17 @@
-import * as request from 'request-promise';
 import { BaseElastic } from './base-elastic';
-import { join } from 'bluebird';
 
 export class BasicSearch extends BaseElastic {
     run() {
         this.recreateIndex(this.getMapping())
-            .then(() => this.indexDocuments(this.getDocuments()));
+            .then(() => this.indexDocuments(this.getDocuments())
+                .then(() => this.search(this.getQuery())
+                    .then(result => console.log(this.getSearchHits(result)))));
     }
 
     private getMapping() {
         return {
             properties: {
-                description: { type: "text" }
+                description: { type: 'text' }
             }
         };
     }
@@ -22,7 +22,46 @@ export class BasicSearch extends BaseElastic {
             { description: 'dog dog' },
             { description: 'dog dog dog' },
             { description: 'dog dog dog dog' },
-            { description: 'dog cat' }
+            { description: 'dog cat mouse pig zebra' },
+            { description: 'zebra zebra zebra zebra zebra zebra zebra zebra zebra zebra' }
         ];
+    }
+
+    private getQuery() {
+        const query = 'dog zebra';
+        return {
+            query: {
+                bool: {
+                    should: [ this.getStrictQuery(query), this.getLooseQuery(query) ]
+                }
+            }
+        }
+    }
+
+    private getLooseQuery(query) {
+        return {
+            multi_match: {
+                query,
+                fields: ['description']
+            }
+        }
+    }
+
+    private getStrictQuery(query) {
+        return {
+            multi_match: {
+                type: 'phrase',
+                slop: 3,
+                query,
+                fields: ['description'],
+                boost: 100
+            }
+        };
+    }
+
+    private getSearchHits(result) {
+        return (<any[]>result.hits.hits)
+            .map(i => i._source)
+            .map(i => i.description);
     }
 }
